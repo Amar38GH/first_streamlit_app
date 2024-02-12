242af5e5b8b62d792f4f8c4003ff860c76c187b0
Amar38GH <159783861+Amar38GH@users.noreply.github.com>
2024-02-12 21:44:20 +0530

    Update streamlit_app.py
    Updation

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-
+streamlit.text('Hard-Boiled Free-Range Egg')
